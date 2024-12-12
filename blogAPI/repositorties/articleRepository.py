de81c71d3ab3db13b214db73a68738afe92bf529
Pranshu Kumar <pranshu.kumar@deltax.com>
2024-12-12 16:30:46 +0530

    fix get article issue

--- blogAPI/repositorties/articleRepository.py
+++ blogAPI/repositorties/articleRepository.py
@@ -7,14 +7,14 @@ class ArticleRepository(BaseRepo):
     
     def get(self,id):
         with self.engine.connect() as conn: 
-            query = f"select Id ,Title,Content from article where Id = {id}" 
+            query = f"select id ,title,content from article where Id = {id}" 
             result =  conn.execute(text(query))  
             row = result.fetchone() 
-        return Article.model_construct(row)
+        return Article.model_construct({'id','title','content'},**row._mapping)
     
     def createArticle(self,article:Article):
         with self.engine.connect() as conn: 
-            query = f"INSERT INTO article (Title, Content) VALUES ('{article.title}', '{article.content}')"
+            query = f"INSERT INTO article (title, content) VALUES ('{article.title}', '{article.content}')"
             conn.execute(text(query))
             id = conn.execute(text("SELECT last_insert_id()")).fetchone()[0]
             conn.commit()
